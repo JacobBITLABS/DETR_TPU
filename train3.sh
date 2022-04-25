d31eccc9f6e01150fc68193ac616e7f5b263fb11
Jacob Nielsen <jacob.nielsen111@gmail.com>
2022-04-25 13:45:00 +0200

    removed non-existing num_classes arg

--- train3.sh
+++ train3.sh
@@ -5,7 +5,6 @@ python3 main.py \
   --coco_path ../DroneVis_DETR/ \
   --output_dir /home/jacobnielsen/DETR_run/DETR_TPU/output \
   --resume "detr-r50_no-class-head.pth" \
-  --num_classes 91 \
   --lr 1e-5 \
   --lr_backbone 1e-6 \
   --epochs 2
