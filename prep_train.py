251ab652e35fe032c377aaa586cd1018a7599d57
Jacob Nielsen <jacob.nielsen111@gmail.com>
2022-04-25 13:38:59 +0200

    tryy

---
+++ prep_train.py
@@ -0,0 +1,33 @@
+"""
+Module to run before the main.py for training\n
+Definedd in train2.sh
+"""
+import torch, torchvision
+
+def load_model():
+    print("Loading Model")
+    pretrained = True
+
+    if pretrained:
+        # Get pretrained weights
+        checkpoint = torch.hub.load_state_dict_from_url(
+                    url='https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth',
+                    map_location='cpu',
+                    check_hash=True)
+
+        # Remove class weights
+        del checkpoint["model"]["class_embed.weight"]
+        del checkpoint["model"]["class_embed.bias"]
+
+        # SaveOGH
+        torch.save(checkpoint,
+                'detr-r50_no-class-head.pth')
+
+
+def prep_train():
+    print("Prepping Training Environment")
+    load_model()
+
+    print("Done.")
+
+prep_train()
---
+++ train3.sh
@@ -0,0 +1,11 @@
+python3 prep_train.py
+
+python3 main.py \
+  --dataset_file "coco" \
+  --coco_path ../DroneVis_DETR/ \
+  --output_dir /home/jacobnielsen/DETR_run/DETR_TPU/output \
+  --resume "detr-r50_no-class-head.pth" \
+  --num_classes 91 \
+  --lr 1e-5 \
+  --lr_backbone 1e-6 \
+  --epochs 2
