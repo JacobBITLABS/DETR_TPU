c095853d789aa1efe885ea97d8b366e8c53a2649
Jacob Nielsen <jacob.nielsen111@gmail.com>
2022-04-25 10:11:03 +0200

    cigbutts test

--- train2.sh
+++ train2.sh
@@ -1,6 +1,6 @@
 python3 main.py \
         --device tpu  \
         --output /home/jacobnielsen/DETR_run/DETR_TPU/output \
-        --coco_path ../DroneVis_DETR/ 
+        --coco_path ../cig_butts_detr/ 
         #--resume https://dl.fbaipublicfiles.com/detr/detr-r101-2c7b67e5.pth \
        
