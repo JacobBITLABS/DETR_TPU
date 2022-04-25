c513f78c6a9524251c54123e7b3f67f373aa84df
Jacob Nielsen <jacob.nielsen111@gmail.com>
2022-04-25 07:58:01 +0200

    train2 change

--- train2.sh
+++ train2.sh
@@ -1,5 +1,5 @@
 python DETR_TPU/main.py \
-         --device tpu  
-         -—output /output \
+        --device tpu  \
+        --output /output \
         --resume https://dl.fbaipublicfiles.com/detr/detr-r101-2c7b67e5.pth \
         --coco_path ../DroneVis_DETR/ 
