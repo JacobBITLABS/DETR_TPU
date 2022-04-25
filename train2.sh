028f01abd39c8df8f4781156730e21a1b37de2cb
Jacob Nielsen <jacob.nielsen111@gmail.com>
2022-04-25 10:29:33 +0200

    ihiwe

--- train2.sh
+++ train2.sh
@@ -2,5 +2,6 @@ python3 main.py \
         --device tpu  \
         --output /home/jacobnielsen/DETR_run/DETR_TPU/output \
         --coco_path ../cig_butts_detr/ 
+        --epochs 10
         #--resume https://dl.fbaipublicfiles.com/detr/detr-r101-2c7b67e5.pth \
        
