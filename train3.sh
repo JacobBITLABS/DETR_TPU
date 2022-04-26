f54574667ae4e06cdc81a0447a654cc82054fa53
Jacob Nielsen <jacob.nielsen111@gmail.com>
2022-04-26 08:45:25 +0200

    wilder

--- train3.sh
+++ train3.sh
@@ -3,7 +3,7 @@
 python3 main.py \
   --device tpu  \
   --dataset_file "coco" \
-  --coco_path ../DroneVis_DETR/ \
+  --coco_path ../wilder_dataset/ \
   --output_dir /home/jacobnielsen/DETR_run/DETR_TPU/output \
   --resume 'https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth' \
   --lr 1e-5 \
